ffa6be190c27712753e3a87e8fe955203aee2f72
Nishant harinkhede <83934580+nishant-harinkhede12@users.noreply.github.com>
2021-07-23 13:03:23 +0530

    Add files via upload

--- LendingClub.py
+++ LendingClub.py
@@ -12,6 +12,7 @@ y = data_final.iloc[:,-1].values
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size =1/3, random_state=42)
 
+####################################################################################
 from sklearn.tree import DecisionTreeClassifier
 DT = DecisionTreeClassifier(criterion='gini',max_depth=None)
 
@@ -28,4 +29,21 @@ cm = confusion_matrix(y_test,y_pred)
 print("'confusion_matrix'",cm)
 
 ac = accuracy_score(y_test,y_pred)
+print("accuracy_score",ac)
+####################################################################################
+from sklearn.ensemble import RandomForestClassifier
+
+RF = RandomForestClassifier(n_estimators=600)
+
+RF.fit(x_train,y_train)
+
+y_pred_RM=RF.predict(x_test)
+
+cr = classification_report(y_test,y_pred_RM)
+print("'classification_report'",cr)
+
+cm = confusion_matrix(y_test,y_pred_RM)
+print("'confusion_matrix'",cm)
+
+ac = accuracy_score(y_test,y_pred_RM)
 print("accuracy_score",ac)
